bfd297a648f660a26a71d955812893409abb1a3e
andresfz85 <162514066+andresfz85@users.noreply.github.com>
2024-03-17 12:31:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ if hist_button2: # al hacer clic en el botón
     st.write('Creación de gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
             
     # crear un grágico
-    fig2 = px.plotly_chart(car_data, x="odometer")
+    fig2 = px.scatter(car_data, x="odometer")
         
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig2, use_container_width=True)
